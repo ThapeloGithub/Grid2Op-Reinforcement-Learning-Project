e9d5a75c5f00d7704d2a5093b90bdbf2624f9f4e
ThapeloGithub <167798361+ThapeloGithub@users.noreply.github.com>
2024-10-21 16:08:21 +0200

    Add files via upload

---
+++ SAC&PPO/files/run_sac_ppo.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from nbconvert import NotebookExporter
+from nbconvert.preprocessors import ExecutePreprocessor
+import nbformat
+
+def run_notebook(notebook_path):
+    # Load the notebook
+    with open(notebook_path) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    # Create an executor
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+
+    # Execute the notebook
+    try:
+        ep.preprocess(nb, {'metadata': {'path': os.path.dirname(notebook_path)}})
+        print(f"Successfully ran {notebook_path}")
+    except Exception as e:
+        print(f"Error while executing the notebook: {e}")
+
+if __name__ == "__main__":
+
+    notebook_path = "files/sac&ppo.ipynb"
+    run_notebook(notebook_path)
